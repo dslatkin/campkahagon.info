7f5033026089dcc58f389861fe8663963e975ab6
Daniel Slatkin <danielslatkin@gmail.com>
2023-06-27 04:29:38 +0000

    add mdsvex

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,1 +1,30 @@
 Todo
+
+
+# Markdown heading 1
+
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia sed quisquam
+vero reiciendis natus distinctio reprehenderit, aliquam eos ducimus quos aut
+asperiores sapiente! Incidunt impedit quis, architecto accusamus quidem
+reiciendis.
+
+## Markdown heading 2
+
+Asperiores necessitatibus maiores, enim eaque distinctio aspernatur expedita,
+numquam vel iusto quo sequi vitae. Voluptatum dolorum rem asperiores ducimus
+iusto tempore eos in, nesciunt est voluptate fugiat vel, assumenda perspiciatis.
+Iure sed hic excepturi doloremque eaque maxime adipisci voluptatibus rem
+molestias! Voluptatem, maiores! Fugiat, possimus fugit consectetur sit harum
+voluptate? Totam quae assumenda iure mollitia fugit nesciunt deserunt esse
+atque.
+
+## Markdown heading 3
+
+Laborum, enim sint iusto, nulla blanditiis illo tempore ullam, neque quas
+inventore optio ut. Assumenda officia omnis earum error! Hic totam modi sed
+reiciendis ullam asperiores eligendi sequi cum quisquam. Delectus deserunt minus
+dolores quos accusamus culpa odio temporibus! Autem doloremque corporis officia
+nihil totam nostrum fuga officiis tempora reiciendis sint, tempore quos! Officia
+natus odit accusamus dolorum, doloribus a. Maiores ipsa magni ab, illo magnam, a
+dolorem ea veniam sequi sapiente enim temporibus sint nobis non fugiat unde
+omnis iure? Maiores laboriosam sed iusto officiis a nobis veritatis? Placeat?
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Enable preprocessing <style lang="postcss"> for Tailwind generations
-    preprocess: vitePreprocess(),
+    extensions: ['.svelte'],
+
+    preprocess: [
+        mdsvex({
+            extensions: ['.svelte'],
+        }),
+        vitePreprocess(),
+    ],
 
     kit: {
         adapter: adapter(),

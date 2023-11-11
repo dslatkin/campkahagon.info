c27f3e09fe66b426893b7a750b3f3b0ecc5a91f3
Daniel Slatkin <danielslatkin@gmail.com>
2023-11-11 23:05:33 +0000

    sync svelte.config.js with upstream

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    extensions: ['.svelte'],
-
     preprocess: [vitePreprocess()],
 
     kit: {

72381b98769b5b2f247351d102278d9cf2c0579a
Daniel Slatkin <danielslatkin@gmail.com>
2023-11-11 20:19:02 +0000

    add comment to FlickrEmbed

--- src/lib/FlickrEmbed.svelte
+++ src/lib/FlickrEmbed.svelte
@@ -11,6 +11,33 @@
     // export let slideshow = false
 
     onMount(() => {
+        /*
+        Sample embed code from flickr with header, footer, slideshow turned on.
+        
+        -   Slide-show is enabled from `data-context`
+        -   I'm not sure if width, height is given dynamically based on the
+            photo dimensions (I would guess yes)
+        -   Script tags are replaced to not confuse the Svelte compiler by
+            having them in a comment.
+
+        <a data-flickr-embed="true" data-header="true" data-footer="true"
+            data-context="true"
+            href="https://www.flickr.com/photos/28156252@N05/2801012285/in/album-72157606975076827/"
+            title="Yvonne camp pix 7"
+        >
+            <img
+                src="https://live.staticflickr.com/3250/2801012285_4acdcc959c_b.jpg"
+                width="1023"
+                height="674"
+                alt="Yvonne camp pix 7"
+            />
+        </a>
+        <SCRIPT-START>
+            async
+            src="//embedr.flickr.com/assets/client-code.js"
+            charset="utf-8"
+        </SCRIPT-END>
+        */
         const script = document.createElement('script');
         script.src = 'https://embedr.flickr.com/assets/client-code.js';
         script.async = true;

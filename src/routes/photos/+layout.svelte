d9ec5ebc70fc30b5beb256d610f0b0fde3d69822
Daniel Slatkin <danielslatkin@gmail.com>
2024-08-06 20:44:09 +0000

    remove sidebar nav groups on photographs page

--- src/routes/photos/+layout.svelte
+++ src/routes/photos/+layout.svelte
@@ -10,37 +10,30 @@
             {
                 href: '/photos/saryl',
                 label: 'Saryl Radwin',
-                group: 'Old Collection',
             },
             {
                 href: '/photos/yvonne',
                 label: 'Yvonne Rosenstein',
-                group: 'Old Collection',
             },
             {
                 href: '/photos/andy',
                 label: 'Andy Sachs',
-                group: 'Old Collection',
             },
             {
                 href: '/photos/diane',
                 label: 'Diane Fellows Zimmer',
-                group: 'New Collection',
             },
             {
                 href: '/photos/marty',
                 label: 'Marty Bachrach',
-                group: 'New Collection',
             },
             {
                 href: '/photos/stuart',
                 label: 'Stuart Harrow',
-                group: 'New Collection',
             },
             {
                 href: '/photos/unknown',
                 label: 'Unknown',
-                group: 'New Collection',
             },
         ]}
     />

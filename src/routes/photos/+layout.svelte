dd0c5da18c120c996e5c481e18489be2b238ddea
Daniel Slatkin <danielslatkin@gmail.com>
2024-08-06 20:53:35 +0000

    add "from" heading on photographs sidenav

--- src/routes/photos/+layout.svelte
+++ src/routes/photos/+layout.svelte
@@ -10,30 +10,37 @@
             {
                 href: '/photos/saryl',
                 label: 'Saryl Radwin',
+                group: 'From',
             },
             {
                 href: '/photos/yvonne',
                 label: 'Yvonne Rosenstein',
+                group: 'From',
             },
             {
                 href: '/photos/andy',
                 label: 'Andy Sachs',
+                group: 'From',
             },
             {
                 href: '/photos/diane',
                 label: 'Diane Fellows Zimmer',
+                group: 'From',
             },
             {
                 href: '/photos/marty',
                 label: 'Marty Bachrach',
+                group: 'From',
             },
             {
                 href: '/photos/stuart',
                 label: 'Stuart Harrow',
+                group: 'From',
             },
             {
                 href: '/photos/unknown',
                 label: 'Unknown',
+                group: 'From',
             },
         ]}
     />
